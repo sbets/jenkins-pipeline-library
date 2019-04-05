7f0eae0970a6ee68ac879f289017af147dd5d946
Serhii Betsmanovskyi <48160048+serhii-dog@users.noreply.github.com>
2019-04-05 11:27:54 +0300

    Create DeployApplication.groovy

---
+++ DeployApplication.groovy
@@ -0,0 +1,34 @@
+#!groovy
+
+try {
+    config = readYaml file: 'config.yaml'
+} catch (e) {
+    println("Unexpected error: ${e}")
+    currentBuild.result = 'UNSTABLE'
+}
+
+println config.weight
+
+node() {
+    
+    stage('Fetch code and check ENV') {
+        
+        sh 'env'
+        def BRANCH_NAME = "${env.BRANCH_NAME}"
+        println BRANCH_NAME
+        try {
+            tagVersion = sh(script: 'git tag -l --points-at HEAD', 
+                              returnStdout: true).trim()
+            println tagVersion
+            if (tagVersion) {
+                environment = "QA"
+                println(environment)
+            } else {
+                environment = "DEV"
+                println(environment)
+            }
+        } catch (e) {
+            println(e)
+        }            
+    }
+}
